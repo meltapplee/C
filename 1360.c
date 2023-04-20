792d39cc9156a436bf72a3906f04f0880ee58bc2
soohyeon <qpsxhf410@outlook.kr>
2023-04-20 18:27:44 +0900

    [codeup]1360
    1360 : 숫자 피라미드 2

---
+++ 1360.c
@@ -0,0 +1,15 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+int main()
+{
+	int n;
+	scanf("%d", &n);
+	for (int i = n; i >= 1; i--)
+	{
+		for (int j = 1; j <= i; j++)
+		{
+			printf("%d ", i);
+		}printf("\n");
+	}
+}
