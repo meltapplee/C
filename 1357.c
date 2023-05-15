3d0a3f75be54d2a72b0686251b38c138b9519455
soohyeon <qpsxhf410@outlook.kr>
2023-05-15 23:38:27 +0900

    [coduep]1357
    1357 : 삼각형 출력하기 4

---
+++ 1357.c
@@ -0,0 +1,18 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+void main()
+{
+    int n;
+    scanf("%d", &n);
+    for (int i = 1; i <= n; i++)
+    {
+        for (int j = 1; j <= i; j++) printf("*");
+        printf("\n");
+    }
+    for (int i = 1; i <= n - 1; i++)
+    {
+        for (int j = n - i; j > 0; j--) printf("*");
+        printf("\n");
+    }
+}
