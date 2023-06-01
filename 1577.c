1005cae489df25c9488907607dca2bf982b3e8b0
soohyeon <qpsxhf410@outlook.kr>
2023-06-01 23:35:49 +0900

    [codeup]1577
    1577 : (함수 작성) 절댓값 함수 1

---
+++ 1577.c
@@ -0,0 +1,16 @@
+//#define _CRT_SECURE_NO_WARNINGS
+//#include <stdio.h>
+//
+//int myabs(int n)
+//{
+//    if (n < 0) n *= -1;
+//    return n;
+//}
+//
+//main()
+//{
+//    int a;
+//    scanf("%d", &a);
+//    printf("%d", myabs(a));
+//    return 0;
+//}
