4e8a06786bde4d8dc9167ee794e90e412aa7c1a5
soohyeon <qpsxhf410@outlook.kr>
2023-06-01 23:37:11 +0900

    [codeup]1580
    1580 : (함수 작성) 원의 넓이

---
+++ 1580.c
@@ -0,0 +1,15 @@
+//#define _CRT_SECURE_NO_WARNINGS
+//#include <stdio.h>
+//
+//float circle(int a)
+//{
+//    return (a * a * 3.14);
+//}
+//
+//main()
+//{
+//    int r;
+//    scanf("%d", &r);
+//    printf("%.2f", circle(r));
+//    return 0;
+//}
