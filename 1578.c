c19afd9e11a7ebe489b3f3b85956433d56aaca6c
soohyeon <qpsxhf410@outlook.kr>
2023-06-01 23:36:09 +0900

    [codeup]1578
    1578 : (함수 작성) 최댓값 함수

---
+++ 1578.c
@@ -0,0 +1,15 @@
+//#define _CRT_SECURE_NO_WARNINGS
+//#include <stdio.h>
+//int mymax(int n1, int n2)
+//{
+//    if (n1 >= n2) return n1;
+//    else return n2;
+//}
+//
+//main()
+//{
+//    int a, b;
+//    scanf("%d %d", &a, &b);
+//    printf("%d", mymax(a, b));
+//    return 0;
+//}
