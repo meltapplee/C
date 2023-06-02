a8ad92d5b0b20f4ea57ec72e194388f30cb627be
soohyeon <qpsxhf410@outlook.kr>
2023-06-02 23:55:14 +0900

    [codeup]1602
    1602 : 절대값 함수

---
+++ 1602.c
@@ -0,0 +1,16 @@
+//#define _CRT_SECURE_NO_WARNINGS
+//#include <stdio.h>
+//
+//double ABS(double a)
+//{
+//	if (a < 0) return -a;
+//	else return a;
+//}
+//
+//int main()
+//{
+//	double n;
+//	scanf("%lf", &n);
+//	printf("%.10g", ABS(n));
+//	return 0;
+//}
