36add37449cd00967eea0f331791250a85519216
soohyeon <qpsxhf410@outlook.kr>
2023-06-06 23:09:23 +0900

    [codeup]1163
    1163 : 당신의 사주를 봐 드립니다 2 해결

---
+++ 1163.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main()
+{
+	int year, month, day;
+	scanf("%d %d %d", &year, &month, &day);
+	if (((year + month + day) / 100) % 2 == 0) printf("대박");
+	else printf("그럭저럭");
+	return 0;
+}
